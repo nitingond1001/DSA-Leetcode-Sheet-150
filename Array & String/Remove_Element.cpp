cdafe476aa9ef6d2268c853b0ee844538a4425c8
Nitin Gond <nitintechy80@gmail.com>
2025-09-30 22:22:58 +0530

    Ques: 02

---
+++ Array & String/Remove_Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int ans = 0;
+
+        if(n == 0){
+            return 0;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] != val){
+                nums[ans] = nums[i];
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
