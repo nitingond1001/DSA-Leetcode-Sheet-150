a1b2a3c48101fdff8d5673f2741c67551398717e
Nitin Gond <nitintechy80@gmail.com>
2025-10-01 21:28:34 +0530

    Ques: 06

---
+++ Array & String/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int minPrice = INT_MAX;
+        int maxProfit = INT_MIN;
+
+        for(int i = 0; i < n; i++){
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, (prices[i] - minPrice));
+        }
+        return maxProfit;
+    }
+};
