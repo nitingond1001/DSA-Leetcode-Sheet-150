272f7428f50025e952d01591f7acd3d498f8f126
Nitin Gond <nitintechy80@gmail.com>
2025-10-19 19:41:17 +0530

    Ques: 88

---
+++ Binary Search Tree/Validate_Binary_Search_Tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool isValid(TreeNode* node, long min, long max){
+    if(node == nullptr){
+        return true;
+    }
+    if(node->val <= min || node->val >= max) return false;
+    return isValid(node->left, min, node->val) && isValid(node->right, node->val, max);
+}
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, LONG_MIN, LONG_MAX);
+    }
+};
