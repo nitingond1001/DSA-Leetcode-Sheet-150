2ceccb0640b99afb6e82364e09b9f6ef85adc51e
Nitin Gond <nitintechy80@gmail.com>
2025-10-28 22:00:18 +0530

    Ques: 121

---
+++ Heap/Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+       priority_queue<int,vector<int>,greater<int>>minHeap;
+       
+       for(auto &num:nums){
+            if(minHeap.size()<k){
+                minHeap.push(num);
+            }
+            else if(minHeap.top()<num){
+                minHeap.pop();
+                minHeap.push(num);
+            }
+       }
+       return minHeap.top();
+    }
+};
