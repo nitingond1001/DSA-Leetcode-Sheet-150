61e48485c42db21ba22fe2cb91940d33d2b150d3
Nitin Gond <nitintechy80@gmail.com>
2025-10-28 22:02:13 +0530

    Ques: 123

---
+++ Heap/Find_K_Pairs_with_Smallest_Sums.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> result;
+        if(nums1.empty() || nums2.empty()) return result;
+
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> minHeap;
+
+        for(int i = 0; i < nums1.size() && i < k; i++){
+            minHeap.push({nums1[i] + nums2[0], i, 0});
+        }
+
+        while(k-- > 0 && !minHeap.empty()){
+            auto [sum, i, j] = minHeap.top();
+            minHeap.pop();
+            result.push_back({nums1[i], nums2[j]});
+
+            if(j + 1 < nums2.size()){
+                minHeap.push({nums1[i] + nums2[j+1], i, j+1});
+            }
+        }
+        return result;
+    }
+};
