94b99cb1607c634d135eec33bf8ac6574ddec19f
Nitin Gond <nitintechy80@gmail.com>
2025-11-02 21:40:52 +0530

    Ques: 145

---
+++ Multidimensional DP/Longest_Palindromic_Substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string expandFromCenter(string &s, int left, int right){
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return s.substr(left + 1, right - left - 1);
+    }
+
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int n = s.size();
+        string longest = "";
+
+        for (int i = 0; i < n; i++) {
+            string odd = expandFromCenter(s, i, i);
+            string even = expandFromCenter(s, i, i + 1);
+
+            if (odd.length() > longest.length()) longest = odd;
+            if (even.length() > longest.length()) longest = even;
+        }
+        return longest;
+    }
+};
